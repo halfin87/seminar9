8e8710eed06773e8edd7d3232f3793db5d848f87
halfin rodion <halfin87@mail.ru>
2023-03-24 15:18:27 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = 4;
+int n = 8;
+Console.WriteLine(GetSumNubers(m,n));
+int GetSumNubers(int m, int n)
+{
+  if (m == n)
+  {
+    return m;
+  }
+  return n += GetSumNubers(m, n - 1);
+}
